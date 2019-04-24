ad2c71a5aeab9059f9f7482887ed8352bd38ec2a
xani <jalexandre.xavier@gmail.com>
2019-04-24 06:38:25 +0100

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 var config = {
-  apiKey: "AIzaSyDTA15Tke9ro-bc726Pla4kLBgzOWfmz74",
-  authDomain: "portocarodos-93f3e.firebaseapp.com",
-  databaseURL: "https://portocarodos-93f3e.firebaseio.com",
-  projectId: "portocarodos-93f3e",
-  storageBucket: "portocarodos-93f3e.appspot.com",
-  messagingSenderId: "671294892710"
+  apiKey: "/* Your apiKey */",
+  authDomain: /* Your authDomain */,
+  databaseURL:  /* Your databaseURL */,
+  projectId: /* Your projectId */,
+  storageBucket: /* Your storageBucket */,
+  messagingSenderId: /* Your messagingSenderId */
 };
 
 firebase.initializeApp(config);
