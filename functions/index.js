327b58a3c14f395a329c89621bd959814c027b60
Alexandre Xavier <jalexandre.xavier@gmail.com>
2019-06-07 17:55:14 +0100

    start to build funtion to colect regatas to db

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@
 // Import and initialize the Firebase Admin SDK.
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+let cheerio = require("cheerio");
 
 //send email to confirme
 const nodemailer = require("nodemailer");
@@ -28,6 +29,7 @@ const mailTransport = nodemailer.createTransport({
 });
 
 const APP_NAME = "PortocaroDos";
+const url = "http://www.rfev.es/default/regatas/calendario/month/06/year/2019";
 
 exports.sendWelcomeEmail = functions.auth.user().onCreate(user => {
   // [END onCreateTrigger]
@@ -84,3 +86,53 @@ async function sendWelcomeEmail(email, displayName) {
   console.log("Email de agradecimento ao novo assinante:", email);
   return null;
 }
+
+const request = require("request-promise");
+
+// This is the URL that we will callback and send the content of the updated data node.
+// As an example we're using a Request Bin from http://requestb.in
+// TODO: Make sure you create your own Request Bin and change this URL to try this sample.
+
+// Reads the content of the node that triggered the function and sends it to the registered Webhook
+// URL.
+exports.webhook = functions.database
+  .ref("/hooks/{hookId}")
+  .onCreate(async snap => {
+    const response = await request({
+      uri: WEBHOOK_URL,
+      method: "POST",
+      json: true,
+      body: snap.val(),
+      resolveWithFullResponse: true
+    });
+    if (response.statusCode >= 400) {
+      throw new Error(`HTTP Error: ${response.statusCode}`);
+    }
+    if (response.status === 200) {
+      var html = response.data;
+      let $ = cheerio.load(html);
+      var regatas = [];
+      var dataInicio = "";
+
+      $(".sixteen").each(function(i, elem) {
+        regatas[i] = {
+          title: $(this)
+            .find("h3.title")
+            .text()
+            .trim(),
+          dates: $(this)
+            .find(".race-date")
+            .text()
+            .trim(),
+          icon: $(this)
+            .find("img")
+            .attr("src"),
+          cla: $(this)
+            .find(".sailclass-icons ")
+            .children()
+            .attr("title")
+        };
+      });
+    }
+    console.log("SUCCESS! Posted", snap.ref);
+  });
